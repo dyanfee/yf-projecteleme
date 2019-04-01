bc3ee1f06270e1f8bae6ab17257eced841f28574
dyanfee <dyanfee@gmail.com>
2019-04-01 18:58:15 +0800

    create projects

---
+++ getFromEleme.py
@@ -0,0 +1,10 @@
+import requests
+import time
+import threading
+import pymysql
+import json
+
+
+class GetFromEleme():
+    def get(self):
+        print("this is get Func!")
---
+++ main.py
@@ -0,0 +1,7 @@
+import getFromEleme as get
+
+if __name__ == "__main__":
+    print("PC start!")
+    print("1：测试requets")
+    te = get.GetFromEleme()
+    
