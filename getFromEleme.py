44ef50cc2642f26311633156ef0cdd06e37b8625
dyanfee <dyanfee@gmail.com>
2019-04-01 19:37:45 +0800

    test

--- getFromEleme.py
+++ getFromEleme.py
@@ -8,4 +8,15 @@ import json
 class GetFromEleme():
     def get(self):
         print("this is get Func!")
-        res = requests.get('')
+        res = requests.post('https://h5.ele.me/hongbao/#hardware_id=&is_lucky_group=True&lucky_number=8&track_id=&platform=0&sn=2a2e2d990f3b004d&theme_id=569&device_id=&refer_user_id=12034332')
+        res.encoding = 'utf-8'
+        print(res.text)
+        with open("res.html",'w+') as f:
+            f.writelines(res.text)
+            f.close()
+        print(res.content)
+
+
+if __name__ == "__main__":
+    test = GetFromEleme()
+    test.get()
